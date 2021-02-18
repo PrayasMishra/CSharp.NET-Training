ed4d6992472e67bc638fb8ac6481cc76d92b1560
Prayas Mishra <40265470+PrayasMishra@users.noreply.github.com>
2021-02-18 12:30:16 +0530

    Add files via upload

---
+++ 2-16-2021/Capgemini.CSharp.Day3/Capgemini.CSharp.Day3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.CSharp.Day3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine("enter name: ");
+			string name = ReadLine();
+			WriteLine("enter password: ");
+			string pwd = ReadLine();
+
+			WriteLine($"name: {name}\tpassword: {pwd}");
+			ReadKey();
+		}
+	}
+}
