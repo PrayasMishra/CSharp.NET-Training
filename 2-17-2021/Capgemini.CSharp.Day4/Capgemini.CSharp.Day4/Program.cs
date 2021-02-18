a52b56f83aeb0073676642546d63e4f30da5d780
Prayas Mishra <40265470+PrayasMishra@users.noreply.github.com>
2021-02-18 12:31:12 +0530

    Add files via upload

---
+++ 2-17-2021/Capgemini.CSharp.Day4/Capgemini.CSharp.Day4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.CSharp.Day4
+{
+	class ProductDemo
+	{
+		object ProductId, ProductName, Price, Quantity;
+		double AmountPayable;
+        static void Main(string[] args)
+		{
+            ProductDemo pd = new ProductDemo();
+			pd.GetDetails();
+			pd.ShowDetails();
+			ReadKey();
+		}
+
+        private void ShowDetails()
+        {
+			double price = ToDouble(this.Price);
+			int quant = ToInt32(this.Quantity);
+
+			WriteLine($"Product ID: {ToInt32(this.ProductId)}");
+			WriteLine($"Product Name: {this.ProductName.ToString()}");
+			WriteLine($"Price: {price}");
+			WriteLine($"Quantity: {quant}");
+			WriteLine($"AmountPayable: {ToDouble(this.AmountPayable = price*quant)}");
+		}
+
+        private void GetDetails()
+        {
+			WriteLine("enter product id: ");
+			this.ProductId = ReadLine();
+
+			WriteLine("enter product name: ");
+			this.ProductName = ReadLine();
+
+			WriteLine("enter price: ");
+			this.Price = ReadLine();
+
+			WriteLine("enter quantity: ");
+			this.Quantity = ReadLine();
+		}
+
+
+    }
+}
